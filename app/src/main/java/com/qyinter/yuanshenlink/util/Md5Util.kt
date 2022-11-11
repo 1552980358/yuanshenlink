1f38bd2d57bde3de6e61094f40fc179325cdb85a
1552980358 <1552980358@qq.com>
2022-11-11 10:10:32 +0800

    Md5Util.kt: Optimize method getMD5()

--- app/src/main/java/com/qyinter/yuanshenlink/util/Md5Util.kt
+++ app/src/main/java/com/qyinter/yuanshenlink/util/Md5Util.kt
@@ -15,22 +15,20 @@ object Md5Util {
      * @param str 原始值
      * @return MD5 值
      */
-    fun getMD5(str: String): String? {
-        try {
+    fun getMD5(str: String) = try {
             // 生成一个MD5加密计算摘要
-            val md = MessageDigest.getInstance(MESSAGE_DIGEST_MD5)
-            // 计算md5函数
-            md.update(str.toByteArray())
-            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
-            val result = BigInteger(1, md.digest()).toString(16)
-            return when {
-                result.length >= MD5_MIN_LENGTH -> result
-                else -> result.padStart(MD5_MIN_LENGTH, MD5_FILLING)
+            MessageDigest.getInstance(MESSAGE_DIGEST_MD5).run {
+                // 计算md5函数
+                update(str.toByteArray())
+
+                // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+                // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+                BigInteger(1, digest()).toString(16)
+                    // 大于等于32直接返回, 不足32则补足到32
+                    .padStart(MD5_MIN_LENGTH, MD5_FILLING)
             }
-        } catch (e: Exception) {
-            println(e)
-        }
-        return null
+    } catch (e: Exception) {
+        null
     }
+
 }
